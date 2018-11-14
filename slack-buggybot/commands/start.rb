8b64033c48b2655763856f29f76c97cac5f54122
Ash Furrow <ash@ashfurrow.com>
2018-11-14 15:53:03 -0500

    Ability to start a hackathon.

--- slack-buggybot/commands/start.rb
+++ slack-buggybot/commands/start.rb
@@ -8,9 +8,10 @@ module SlackBuggybot
 
       def self.call(client, data, match)
         url = match['expression']
+        user = client.users[data[:user]]
 
         # Don't let people run more than one event at once.
-        current_events = SlackBuggybot::Database.database[:events].where(owner: data[:user])
+        current_events = database[:events].where(owner: user.id)
         if current_events.count > 0
           client.say(channel: data.channel, text: "You already have an event in progress.")
           return
@@ -18,15 +19,25 @@ module SlackBuggybot
 
         # Not sure why but the expression is surrounded by angle brackets
         url.gsub!(/[\>\<]/, "")
+
         issues = SlackBuggybot::IssueFinder.find(url)
-        user = client.users[data[:user]]
         if issues.empty?
           client.say(channel: data.channel, text: "Couldn't find any issues at #{url}")
         else
           client.say(channel: data.channel, text: "Starting hackathon for #{user.real_name} with #{issues.length} issues!")
+          database.transaction do
+            event_id = database[:events].insert(start: Time.now.utc, owner: user.id)
+            issues.each do |url|
+              database[:bugs].insert(event_id: event_id, url: url)
+            end
+          end
         end
       rescue StandardError => e
         client.say(channel: data.channel, text: "Sorry, an oop happened: #{e.message}.")
+      end
+
+      def self.database
+        SlackBuggybot::Database.database
       end
     end
   end
