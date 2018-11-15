9eabc99609cd738bda83a71eed2fc440960b57cb
Ash Furrow <ash@ashfurrow.com>
2018-11-15 10:10:24 -0500

    Stores channel details and bug resolution.

--- slack-buggybot/commands/start.rb
+++ slack-buggybot/commands/start.rb
@@ -7,8 +7,16 @@ module SlackBuggybot
   module Commands
     class Start < SlackRubyBot::Commands::Base
       def self.call(client, data, match)
-        url = match['expression']
+        channel_name, url = match['expression'].split(" ")
         user = client.users[data[:user]]
+
+        # Channel names come through as "<CE4HP6V8A|buggy-test-channel>"
+        channel_name = channel_name.gsub(/[\>\<\#]/, "").split("|").first
+        channel = client.channels[channel_name]
+        if channel.nil?
+          client.say(channel: data.channel, text: "Couldn't find the channel #{channel_name}")
+          return
+        end
 
         # Don't let people run more than one event at once.
         current_events = Event.where(owner: user.id)
@@ -26,7 +34,7 @@ module SlackBuggybot
         else
           client.say(channel: data.channel, text: "Starting hackathon for #{user.real_name} with #{issues.length} issues!")
           SlackBuggybot::Database.database.transaction do
-            event = Event.new(start: Time.now.utc, owner: user.id)
+            event = Event.new(start: Time.now.utc, owner: user.id, channel_id: channel.id)
             event.save
             issues.each do |url|
               Bug.new(event_id: event.id, url: url).save
--- slack-buggybot/database.rb
+++ slack-buggybot/database.rb
@@ -12,12 +12,19 @@ module SlackBuggybot
             DateTime :start, null: false
             DateTime :end
             String :owner, null: false
+            String :channel_id, null: false
             column 'users', 'text[]', null: false, default: []
           end
         end
 
         unless db.table_exists?("bugs")
-          db.create_enum(:event_state, %w(ready wip done))
+          # Enum options (ready, then wip, then one of these)
+          # > 1. I fixed it
+          # > 2. I added steps to reproduce or documentation
+          # > 3. I verified the bug still exists
+          # > 4. I found the bug in another place and added links
+          # > 5. I didn't do anything, I just want a new bug.
+          db.create_enum(:event_state, %w(ready wip fixed added_docs verified interlinked))
           db.create_table :bugs do
             primary_key :id
             foreign_key :event_id, :events
@@ -27,7 +34,6 @@ module SlackBuggybot
             event_state :state, default: 'ready'
           end
         end
-
         return db
       end
     end
