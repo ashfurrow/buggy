90ef33190454db37f4de8b9f445e567e1b38cdab
Ash Furrow <ash@ashfurrow.com>
2018-11-17 10:55:50 -0500

    Initial unit tests.

---
+++ spec/helpers_spec.rb
@@ -0,0 +1,18 @@
+require 'slack-buggybot/helpers'
+
+describe 'helpers' do
+  describe '#random_fun_emoji' do
+    it 'begins and ends with a colon' do
+      emoji = random_fun_emoji()
+      expect(emoji.start_with?(':')).to be_truthy
+      expect(emoji.end_with?(':')).to be_truthy
+    end
+  end
+
+  describe '#random_emojis' do
+    it 'contains at least 6 colons' do
+      emojis = random_emojis()
+      expect(emojis.count(':')).to be >= 6
+    end
+  end
+end
