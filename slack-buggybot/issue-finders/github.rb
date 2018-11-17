500a55f8f70c304fcb042156f73a31968c3ddbb9
Ash Furrow <ash@ashfurrow.com>
2018-11-17 10:38:44 -0500

    Adds note about bug.

--- slack-buggybot/issue-finders/github.rb
+++ slack-buggybot/issue-finders/github.rb
@@ -5,6 +5,7 @@ module SlackBuggybot
   module IssueFinders
     class GitHub
       def self.find(repo, query = nil)
+        # TODO: Not working on https://github.com/ashfurrow/test-repo/issues?q=is%3Aissue+is%3Aopen+label%3A%22bug+bash%22 because the label has a space in it
         client = Octokit::Client.new(:access_token => ENV['GITHUB_ACCESS_TOKEN'])
         client.auto_paginate = true
 
